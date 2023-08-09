b717ce0fd4f86734f86263fd13e5108d78d36cee
mathiasbernardico <mbernardico27@gmail.com>
2023-08-09 19:14:03 -0300

    1.1

--- js/validacion.js
+++ js/validacion.js
@@ -5,27 +5,40 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
 function Campos_no_vacios(){
-      
     nombre = document.getElementById("nombre").value;
     apellido  = document.getElementById("apellido").value;
     email = document.getElementById("email").value;
     password1  = document.getElementById("password1").value;
     password2 = document.getElementById("password2").value;
     terminos = document.getElementById("terminos").checked;
-
   if (nombre == ''||apellido == ''||email == ''||password1 == ''||password2 == ''||terminos == false) {
-    showAlertError();
     return false;
-  }else if (nombre != ''&&apellido != ''&&email != ''&&password1 != ''&&password2 != ''&&terminos){
+  }else {
     return true;
   }
-}
-function errorContraseña() { 
-    let password1 = document.getElementById("password1").value; let password2 = document.getElementById("password2").value; if ((password1 !== password2)) { 
-        showAlertError(); } 
-    else if (password1 === password2){ 
-        showAlertSuccess() 
+  }
+  
+
+function errorContraseña() {
+     var password1 = document.getElementById("password1").value; var password2 = document.getElementById("password2").value; 
+     if ((password1 !== password2) || (password1.length < 6) || (password2.length < 6)) {
+            return false;
+        } else { 
+            return true; 
+        } 
     } 
-} 
-document.getElementById("regBtn").addEventListener("click", errorContraseña);
+
+
+function verificacion(){
+     if(Campos_no_vacios() && errorContraseña()){ 
+        showAlertSuccess();
+    }else {
+        showAlertError();
+    } 
+}
+
+
+document.getElementById("regBtn").addEventListener("click", verificacion);
+
